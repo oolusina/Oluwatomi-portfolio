931b1afd87ed0122678d1659f80781069d5a9292
Oluwatomi Olusina <oolusina@terpmail.umd.edu>
2020-06-26 22:06:45 -0400

    Corrections from previous pull request

--- walkthroughs/week-4-tdd/intro/src/main/java/com/google/sps/Greeter.java
+++ walkthroughs/week-4-tdd/intro/src/main/java/com/google/sps/Greeter.java
@@ -23,6 +23,7 @@ public class Greeter {
    * Returns a greeting for the given name.
    */
   public String greet(String name) {
+    //clean greeting removes the whitespace and extra characters from the name
     String cleanGreeting = "Hello ";
     for (int n = 0; n < name.length(); n++){
         if (Character.isLetter(name.charAt(n))) cleanGreeting += name.charAt(n);
